362b114ef16753235505e7c330471fba3109faf0
binishmaharjan <legendbinish124@gmail.com>
2021-12-05 02:29:25 +0900

    add token service test file

--- account/service/token_service.go
+++ account/service/token_service.go
@@ -3,8 +3,10 @@ package service
 import (
 	"context"
 	"crypto/rsa"
+	"log"
 
 	"github.com/binishmaharjan/memrizr/account/model"
+	"github.com/binishmaharjan/memrizr/account/model/apperrors"
 )
 
 // TokenService used for injecting an implementation of TokenRepository
@@ -40,27 +42,24 @@ func NewTokenService(c *TSConfig) model.TokenService {
 // If a previous token is included, the previous token is removed from
 // the tokens repository
 func (s *TokenService) NewPairFromUser(ctx context.Context, u *model.User, previousTokenID string) (*model.TokenPair, error) {
-	panic("")
+	idToken, err := generateIDToken(u, s.PrivKey)
+
+	if err != nil {
+		log.Printf("Error generating idToken for uid: %v\n", u.UID, err.Error())
+		return nil, apperrors.NewInternal()
+	}
+
+	refreshToken, err := generateRefreshToken(u.UID, s.RefreshSecret)
+
+	if err != nil {
+		log.Printf("Error generating refreshToken for uid: %v\n", u.UID, err.Error())
+		return nil, apperrors.NewInternal()
+	}
+
+	// TODO: store refresh tokens by calling TokenRepository methods
+
+	return &model.TokenPair{
+		IDToken:      idToken,
+		RefreshToken: refreshToken.SS,
+	}, nil
 }
-
-// // No need to use a repository for idToken as it is unrelated to any data source
-// idToken, err := generateIDToken(u, s.PrivKey)
-
-// if err != nil {
-// 	log.Printf("Error generating idToken for uid: %v. Error: %v\n", u.UID, err.Error())
-// 	return nil, apperrors.NewInternal()
-// }
-
-// refreshToken, err := generateRefreshToken(u.UID, s.RefreshSecret)
-
-// if err != nil {
-// 	log.Printf("Error generating refreshToken for uid: %v. Error: %v\n", u.UID, err.Error())
-// 	return nil, apperrors.NewInternal()
-// }
-
-// // TODO: store refresh tokens by calling TokenRepository methods
-
-// return &model.TokenPair{
-// 	IDToken:      idToken,
-// 	RefreshToken: refreshToken.SS,
-// }, nil
