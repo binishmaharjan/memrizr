812c9f0a6118c5f0d32130251b301c3d29b2ae82
binishmaharjan <legendbinish124@gmail.com>
2022-01-03 12:02:36 +0900

    add set profile image

--- account/model/mocks/user_service.go
+++ account/model/mocks/user_service.go
@@ -2,10 +2,10 @@ package mocks
 
 import (
 	"context"
-
 	"github.com/binishmaharjan/memrizr/account/model"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
+	"mime/multipart"
 )
 
 // MockUserService is a mock type for model.UserService
@@ -69,4 +69,19 @@ func (m *MockUserService) UpdateDetails(ctx context.Context, user *model.User) e
 	}
 
 	return r0
+}
+func (m *MockUserService) SetProfileImage(ctx context.Context, uid uuid.UUID, imageFileHeader *multipart.FileHeader) (*model.User, error) {
+	ret := m.Called(ctx, uid, imageFileHeader)
+
+	var r0 *model.User
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*model.User)
+	}
+
+	var r1 error
+	if ret.Get(1) != nil {
+		r1 = ret.Get(1).(error)
+	}
+
+	return r0, r1
 }
--- account/repository/gc_image_repository.go
+++ account/repository/gc_image_repository.go
@@ -1,6 +1,13 @@
 package repository
 
 import (
+	"context"
+	"fmt"
+	"github.com/binishmaharjan/memrizr/account/model/apperrors"
+	"io"
+	"log"
+	"mime/multipart"
+
 	"cloud.google.com/go/storage"
 	"github.com/binishmaharjan/memrizr/account/model"
 )
@@ -16,4 +23,26 @@ func NewImageRepository(gcClient *storage.Client, bucketName string) model.Image
 		Storage:    gcClient,
 		BucketName: bucketName,
 	}
+}
+
+func (r *gcImageRepository) UpdateProfile(ctx context.Context, objName string, imageFile multipart.File) (string, error) {
+	bckt := r.Storage.Bucket(r.BucketName)
+
+	object := bckt.Object(objName)
+	wc := object.NewWriter(ctx)
+
+	wc.ObjectAttrs.CacheControl = "Cache-Control:no-cache, max-age=0"
+
+	if _, err := io.Copy(wc, imageFile); err != nil {
+		log.Printf("Unable to write file to Google Cloud Storage: %v \n", err)
+		return "", apperrors.NewInternal()
+	}
+
+	if err := wc.Close(); err != nil {
+		return "", fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	imageURL := fmt.Sprintf("https:storage.googleapis.com/%s/%s", r.BucketName, objName)
+
+	return imageURL, nil
 }
